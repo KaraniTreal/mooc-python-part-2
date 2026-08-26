c99981ffb6301f69c9ed08b706123039f1fb6c97
KaraniTreal <andasonkarani@gmail.com>
2026-08-27 02:08:39 +0300

    Completed Part 2 exercises 1-4

---
+++ part02-03_typecasting/src/typecasting.py
@@ -0,0 +1,7 @@
+# Write your solution here
+num = float(input("Please type in a number: "))
+num_int = int(num)
+num_dec = num - num_int
+
+print(f"Integer part: {num_int}")
+print(f"Decimal part: {num_dec}")
