6af8041e3002aaf768d6e89dd5fcee14cd009d87
KaraniTreal <andasonkarani@gmail.com>
2026-08-29 01:58:46 +0300

    Completed Part 2 exercises 7-9:conditinals

---
+++ part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,9 @@
+# Write your solution here
+givenName = input("Given name: ")
+familyName = input("Family name: ")
+streetAddress = input("Street address; ")
+cityAndPostalCode = input("City and postal code: ")
+
+print(givenName, familyName)
+print(streetAddress)
+print(cityAndPostalCode)
